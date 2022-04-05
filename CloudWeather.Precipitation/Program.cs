b846b1b82c569aa6b390246bfe87c797bdea9355
Dan Deng <hui_danny_deng@hotmail.com>
2022-04-05 16:02:27 -0400

    added all required packages to each project

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+app.MapGet("/precipitation/{zip}", (string zip, [FromQuery] int? days)=>{
+    return Results.Ok(days);
+});
 
 app.Run();
