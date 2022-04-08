185021a4e11b431c1495ae58d25a061e4a14e455
Dan Deng <hui_danny_deng@hotmail.com>
2022-04-08 14:10:34 -0400

    added docker compose file to create Postgres data container

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,11 +1,29 @@
+using CloudWeather.Precipitation.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<PrecipitationDbContext>(
+    options =>
+    {
+        options.EnableSensitiveDataLogging();
+        options.EnableDetailedErrors();
+        options.UseNpgsql(builder.Configuration.GetConnectionString("WeatherDb"));
+    }, ServiceLifetime.Transient
+);
+
 var app = builder.Build();
 
-app.MapGet("/precipitation/{zip}", (string zip, [FromQuery] int? days)=>{
-    return Results.Ok(days);
+app.MapGet("/precipitation/{zip}",async (string zip, [FromQuery] int? days, PrecipitationDbContext db)=>{
+    if (days == null || days < 0 || days > 30) {
+        return Results.BadRequest("Please enter a 'days' query paramter or days must be greater than zero and less than 30");
+    }
+    var startdate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await db.PrecipitationSet
+        .Where<Precipitation>(prep => prep.ZipCode == zip && prep.CreateOn > startdate)
+        .ToListAsync<Precipitation>();
+    return Results.Ok(results);
 });
 
 app.Run();
